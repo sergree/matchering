410f5857ce39e4147d2b20630ba90c56d7db4443
Sergree <43672811+sergree@users.noreply.github.com>
2020-01-12 12:56:07 +0300

    GPLv3

--- matchering/__init__.py
+++ matchering/__init__.py
@@ -1,18 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+Matchering
+~~~~~~~~~~~~~~~~~~~
+
+Audio Matching and Mastering Python Library
+
+:copyright: (C) 2016-2020 Sergree
+:license: GPLv3, see LICENSE for more details.
+
+"""
+
+__title__ = 'matchering'
+
+__author__ = 'Sergree'
+__credits__ = ['Sergey Grishakov', 'Igor Isaev', 'Chin Yun Yu']
+__maintainer__ = 'Sergree'
+__email__ = 'wokashi.rg@gmail.com'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright (C) 2016-2020 Sergree'
+
+__version__ = '2.0.0'
+__status__ = 'Development'
+
 from .log.handlers import set_handlers as log
 from .results import Result, pcm16, pcm24
 from .defaults import Config
 from .core import process
 from .loader import load
 from .checker import check
-
-__title__ = 'matchering'
-
-__author__ = 'sergree'
-__credits__ = ['Sergey Grishakov', 'Igor Isaev', 'Chin Yun Yu']
-__maintainer__ = 'sergree'
-__email__ = 'wokashi.rg@gmail.com'
-__license__ = 'GPL'
-__copyright__ = 'Copyright 2016-2020, sergree'
-
-__version__ = '2.0.0'
-__status__ = 'Development'
