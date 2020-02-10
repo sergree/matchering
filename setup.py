dbac25e13299b80f2af7cfd95e41104435c286e0
Sergey Grishakov <infextro@gmail.com>
2020-02-10 21:53:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='matchering',
-    version='2.0.1',
+    version='2.0.2',
     author='Sergree',
     author_email='wokashi.rg@gmail.com',
     description='Audio Matching and Mastering Python Library',
